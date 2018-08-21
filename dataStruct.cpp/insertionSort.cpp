ea050cb653b7dfce9227179414117b181bdd1fee
tama853 <16545536+tama853@users.noreply.github.com>
2018-08-21 00:27:49 -0400

    folder

---
+++ dataStruct.cpp/insertionSort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+
+insertSort()
+{
+
+  for (int i = 2; i < n; i++)
+  {
+    i = 1;
+    while( aj > ai)
+    {
+      i = i + 1;
+    }
+    m = aj;
+    for (int k = 0; k < j-i-1)
+    {
+      aj-k = aj-k-1;  //subscript
+    }
+    ai = m;
+}
+
+int main()
+{
+  
+  return 0;
+}
